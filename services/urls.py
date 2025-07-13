8054f5e241289caabc45416a7e1886efbd578cbb
zizi <124803075+zizisoufi@users.noreply.github.com>
2025-07-13 10:54:20 -0700

    remove problematic subrepo and commit changes

--- services/urls.py
+++ services/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import services
+from .views import services,service_detail, service_null
 
 app_name = "services"
 
 urlpatterns = [
     path("" , services, name = "services"),
+    path("detail/<int:id>" , service_detail, name="service_detail"),
+    path("detail/" , service_null, name="service_null"),
 ]
--- services/views.py
+++ services/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Services, Specials
 def services(request):
-    services = Services.objects.filter(status=True)
+    category = request.GET.get("category")
+    if category:
+        services = Specials.objects.filter(category__name=category, status=True)
+    else:
+         services = Services.objects.filter(status=True)
     specials = Specials.objects.filter(status=True)
     context = {
         
         "services" : services,
-         "specials" : specials,
-    }
+        "specials" : specials,
+     }
     return render(request, "services/services.html", context=context)
+
+def service_detail(request, id): 
+    try:                 
+        service =get_object_or_404(Services, pk=id)
+        context = {
+             "service" : service
+         }
+        return render(request, "services/service-details.html", context=context)
+    except:
+        return render(request, "root/404.html", status=404 )
+    
+    
+def service_null(request):
+    return services(request)
